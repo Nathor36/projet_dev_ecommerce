86995ea7c8ae6348ea4e8dadd736fa7256a1588f
Kyronaade <valentinazouz@outlook.fr>
2025-09-24 10:50:57 +0200

    e

--- PagePanier.php
+++ PagePanier.php
@@ -14,10 +14,30 @@ if (!isset($_SESSION['panier'])) {
     <meta charset="UTF-8">
     <title>Mon Panier </title>
     <link rel="stylesheet" href="style_Panier.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+
 </head>
 <body>
+
+<header class="navbar">
+  <div class="logo">Ma Boutique</div>
+  <nav>
+    <ul>
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="produits.php">Produits</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="panier.php"><i class="fas fa-shopping-cart"></i></a></li>
+    </ul>
+  </nav>
+</header>
+
+
     <h1>Mon Panier</h1>
 
 
+
 </body>
+
+
+
 </html>
