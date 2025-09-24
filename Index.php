a5a7d1fd650f60a98f8c3cbca066b74a97f62baa
Nathor36 <nath.marcelot@gmail.com>
2025-09-24 11:24:41 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -4,13 +4,19 @@
 <head>
  <title>Test Cards</title>
  <link rel="stylesheet" href="style.css">
+ <header class="navbar">
+  <div class="logo">Ma Boutique</div>
+  <nav>
+    <ul>
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="produits.php">Produits</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="panier.php"><i class="fas fa-shopping-cart"></i></a></li>
+    </ul>
+  </nav>
+</header>
 </head>
 <body>
-<div class="topnav">
-  <a class="active" href="#home">Home</a>
-  <a href="contact.php">Contact</a>
-  <a href="PagePanier.php">Panier</a>
-</div>
 <h1>Apple Store</h1>
 <?php foreach($produits as $produit): ?> 
  <div class="card"> 
