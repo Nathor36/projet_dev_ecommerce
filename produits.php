f94b766fb735fe75e2bc532f8bf26a49158a4926
Nathor36 <nath.marcelot@gmail.com>
2025-09-25 10:42:17 +0200

    Update produits.php

---
+++ produits.php
@@ -0,0 +1,18 @@
+<?php include 'includes/card.php'; ?>
+<!DOCTYPE html>
+<html>
+<head>
+ <title>Produits</title>
+ <link rel="stylesheet" href="style.css">
+ <header class="navbar">
+  <div class="logo">Ma Boutique</div>
+  <nav>
+    <ul>
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="produits.php">Produits</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="PagePanier.php"><i class="fas fa-shopping-cart"></i></a></li>
+    </ul>
+  </nav>
+</header>
+</head>
