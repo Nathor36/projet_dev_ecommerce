ba3002e26e3c07a7711f5682babcf50ae3cf6e3f
benji51110 <benjaminlekki.bl@gmail.com>
2025-09-25 12:06:23 +0200

    modif

--- includes/card.php
+++ includes/card.php
@@ -6,7 +6,11 @@ $produits = [
  ['nom' => 'AirPods Pro', 'prix' => 279, 'image' => 'E.jpg', 'description' => 'Écouteurs sans fil', 'categorie' => 'accessoire'],
  ['nom' => 'iPad Air', 'prix' => 699, 'image' => 'F.jpg', 'description' => 'Tablette polyvalente', 'categorie' => 'tablette'],
  ['nom' => 'Rolex Apple Watch', 'prix' => 44449, 'image' => 'D.jpg', 'description' => 'Montre connectée', 'categorie' => 'accessoire'],
+ ['nom' => 'haarppel fortuna watch', 'prix' => 147450, 'image' => 'G.jpg' , 'description' => 'montre de luxe', 'categorie' => 'accessoire'],
  ['nom' => 'Mac Studio', 'prix' => 2299, 'image' => 'B.jpg', 'description' => 'Station ultra-puissante', 'categorie' => 'ordinateur'],
+ ['nom' => 'haarpods' , 'prix' => 1100, 'image' => 'H.jpeg' , 'description' => 'ecouteurs derniere generation', 'categrogie' => 'accessoires'],
+ ['nom' => 'iphone 64 mini' , 'prix' => 2100, 'image' => 'i.webp' , 'description' => 'telephone derniere generation', 'categrogie' => 'smartphone'],
+ ['nom' => 'ipad KIDIZOOM' , 'prix' => 100, 'image' => 'j.jpg' , 'description' => 'tablette du peuple', 'categrogie' => 'tablette'],
 ];
 
 function createCard($nom, $prix, $image, $description, $categorie)
