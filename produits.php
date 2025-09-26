d815c6d0ffde6fc09d6577f4fd5fc5912859819f
Nathor36 <nath.marcelot@gmail.com>
2025-09-26 14:15:43 +0200

    Update produits.php

--- produits.php
+++ produits.php
@@ -20,6 +20,4 @@
 <?php foreach($produits as $produit): ?> 
  <div class="card"> 
  <?= createCard($produit['nom'], $produit['prix'], $produit['image'], $produit['description'], $produit['categorie']) ?>
- <?= createCard($produit['nom'], $produit['prix'], $produit['image'], $produit['description'], $produit['categorie']) ?>
-</div>
 <?php endforeach; ?> 
