0b2a23ca66fa8e20c32f9dcded0b8f9797033957
benji51110 <benjaminlekki.bl@gmail.com>
2025-09-26 15:57:18 +0200

    f

--- produits.php
+++ produits.php
@@ -2,21 +2,24 @@
 <!DOCTYPE html>
 <html>
 <head>
- <title>Produits</title>
- <link rel="stylesheet" href="style.css">
- <header class="navbar">
-  <div class="logo">Haapple Store</div>
-  <nav>
-    <ul>
-      <li><a href="index.php">Accueil</a></li>
-      <li><a href="produits.php">Produits</a></li>
-      <li><a href="contact.php">Contact</a></li>
-      <li><a href="connexion.php">Se connecter</a></li>
-      <li><a href="PagePanier.php"><i class="fas fa-shopping-cart"></i></a></li>
-    </ul>
-  </nav>
-</header>
+    <meta charset="UTF-8">
+    <title>Produits</title>
+    <link rel="stylesheet" href="produit.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
 </head>
+<body>
+    <header class="navbar">
+        <div class="logo">Haapple Store</div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="produits.php" class="active">Produits</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="connexion.php">Se connecter</a></li>
+                <li><a href="PagePanier.php"><i class="fas fa-shopping-cart"></i></a></li>
+            </ul>
+        </nav>
+    </header>
 
 <?php foreach($produits as $produit): ?> 
  <div class="card"> 
