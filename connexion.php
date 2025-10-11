6497c788c773aeecc2dc58a7c0aef07037d940ed
Nathor36 <nath.marcelot@gmail.com>
2025-10-11 17:28:09 +0200

    css

--- connexion.php
+++ connexion.php
@@ -1,41 +1,51 @@
-<html>
-    <head>
-        <meta charset="utf-8">
-        <link rel="stylesheet" href="style_Page_de_connexion.css" media="screen" type="text/css" />
-    </head>
-    <body>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion - Haapple Store</title>
+    <link rel="stylesheet" href="style_Page_de_connexion.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+<body>
 
+    <!-- Navbar -->
     <header class="navbar">
-  <div class="logo">Haapple Store</div>
-  <nav>
-    <ul>
-      <li><a href="index.php">Accueil</a></li>
-      <li><a href="produits.php">Produits</a></li>
-      <li><a href="contact.php">Contact</a></li>
-      <li><a href="connexion.php">Se connecter</a></li>
-      <li><a href="PagePanier.php"><i class="fas fa-shopping-cart"></i></a></li>
-    </ul>
-  </nav>
-</header>
+        <div class="logo">Haapple Store</div>
+        <nav>
+            <ul>
+                <li><a href="index.php">Accueil</a></li>
+                <li><a href="produits.php">Produits</a></li>
+                <li><a href="contact.php">Contact</a></li>
+                <li><a href="connexion.php" class="active">Se connecter</a></li>
+                <li><a href="PagePanier.php"><i class="fas fa-shopping-cart"></i></a></li>
+            </ul>
+        </nav>
+    </header>
 
-        <div id="container">
-            
-            <form action="verification.php" method="POST">
-                <h1>Connexion</h1>
-                <label><b>Nom d'utilisateur</b></label>
-                <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-                <label><b>Mot de passe</b></label>
-                <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-                <input type="submit" id='submit' value='LOGIN' >
-                <?php
-                if(isset($_GET['erreur'])){
-                    $err = $_GET['erreur'];
-                    if($err==1 || $err==2)
-                        echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+    <!-- Formulaire -->
+    <div id="container">
+        <form action="verification.php" method="POST">
+            <h1>Connexion</h1>
+
+            <label for="username"><b>Nom d'utilisateur</b></label>
+            <input type="text" placeholder="Entrer le nom d'utilisateur" id="username" name="username" required>
+
+            <label for="password"><b>Mot de passe</b></label>
+            <input type="password" placeholder="Entrer le mot de passe" id="password" name="password" required>
+
+            <input type="submit" value="LOGIN">
+
+            <?php
+            if(isset($_GET['erreur'])){
+                $err = intval($_GET['erreur']);
+                if($err === 1 || $err === 2){
+                    echo "<p class='error'>Utilisateur ou mot de passe incorrect</p>";
                 }
-                ?>
-            </form>
-        </div>
-        
-    </body>
+            }
+            ?>
+        </form>
+    </div>
+
+</body>
 </html>
