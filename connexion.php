5a0c2e70c61b67b13c7874506ff8fb3f56a61855
Nathor36 <nath.marcelot@gmail.com>
2025-10-11 17:19:20 +0200

    nav bar connexion

--- connexion.php
+++ connexion.php
@@ -1,22 +1,31 @@
 <html>
     <head>
         <meta charset="utf-8">
-        <!-- importer le fichier de style -->
         <link rel="stylesheet" href="style_Page_de_connexion.css" media="screen" type="text/css" />
     </head>
     <body>
+
+    <header class="navbar">
+  <div class="logo">Haapple Store</div>
+  <nav>
+    <ul>
+      <li><a href="index.php">Accueil</a></li>
+      <li><a href="produits.php">Produits</a></li>
+      <li><a href="contact.php">Contact</a></li>
+      <li><a href="connexion.php">Se connecter</a></li>
+      <li><a href="PagePanier.php"><i class="fas fa-shopping-cart"></i></a></li>
+    </ul>
+  </nav>
+</header>
+
         <div id="container">
-            <!-- zone de connexion -->
             
             <form action="verification.php" method="POST">
                 <h1>Connexion</h1>
-                
                 <label><b>Nom d'utilisateur</b></label>
                 <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
-
                 <label><b>Mot de passe</b></label>
                 <input type="password" placeholder="Entrer le mot de passe" name="password" required>
-
                 <input type="submit" id='submit' value='LOGIN' >
                 <?php
                 if(isset($_GET['erreur'])){
