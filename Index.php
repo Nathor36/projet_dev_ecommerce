eb8d2db3636389307ce9718ca0d278671cf34955
Nathor36 <nath.marcelot@gmail.com>
2025-10-23 22:22:11 +0200

    Update Index.php

--- Index.php
+++ Index.php
@@ -21,7 +21,7 @@ require_once 'connexion_bdd.php'
             <li><a href="produits.php">Produits</a></li>
             <li><a href="contact.php">Contact</a></li>
                 <?php if (isset($_SESSION['id_utilisateur'])): ?>
-    <li><span>Bonjour, <?= htmlspecialchars($_SESSION['prenom']) ?> 👋</span></li>
+    <li><span>Bonjour, <?= htmlspecialchars($_SESSION['prenom']) ?></span></li>
     <li><a href="mes_commandes.php">Mes commandes</a></li>
     <li><a href="deconnexion.php">Se déconnecter</a></li>
 <?php else: ?>
