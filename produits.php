04546609122d5b4a896dd0d1aa018f77e47e2a1a
Nathor36 <nath.marcelot@gmail.com>
2025-10-24 08:19:56 +0200

    Update produits.php

--- produits.php
+++ produits.php
@@ -9,7 +9,7 @@ require_once 'connexion_bdd.php';  // fichier de connexion et il contien la vari
     <meta charset="UTF-8">
     <title>Produits</title>
 
-    <link rel="stylesheet" href="produits.css">  <!--relie le ficher css avec se fichier (si quelqu'un lit sa fais un style stp sinon je vais passer ma nuit BOBBY -->
+    <link rel="stylesheet" href="produit.css">  <!--relie le ficher css avec se fichier (si quelqu'un lit sa fais un style stp sinon je vais passer ma nuit BOBBY -->
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">     <!-- Azouz se lien c'est pour ton panier de mort et le relier au site (oui celui-ci) -->
 </head>
